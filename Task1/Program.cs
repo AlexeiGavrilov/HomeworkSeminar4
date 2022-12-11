a30fb63dd6172d4da131c31b84c20a86f262375c
Alex <kccnbnb@mail.ru>
2022-12-11 16:53:44 +0300

    add tsk1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать функцию Math.Pow
+int FindDegreeNumbers (int arg1, int arg2)
+{   
+     int result = 0;
+     int N = arg1;
+
+    for(int i = 1; i  < arg2; i++)
+    { 
+        N = N*arg1;
+        result = N;
+    }
+    return result;
+}
+
+Console.WriteLine("Ведите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите второе число:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int end = FindDegreeNumbers(num1,num2);
+Console.WriteLine(end);
+
+
